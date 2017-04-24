8d9767d7d51df59bba9ccce84df10d200370796d
Katerina <katerinaldg@gmail.com>
2017-04-24 11:23:13 +0100

    Added test back in which is now green

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -17,13 +17,13 @@ describe Display do
 		expect(output.string).to eq("Please enter a number greater than 0\n")
 	end
 
-	# it "returns 'Please enter a number greater than 0'" do
-	# 	input = StringIO.new("0\n1")
-	# 	output = StringIO.new
-	# 	display = Display.new(output, input)
-	# 	display.get_rate_of_return
-	# 	expect(output.string).to eq("Please enter a number greater than 0\n")
-	# end
+	it "returns 'Please enter a number greater than 0'" do
+		input = StringIO.new("0\n1")
+		output = StringIO.new
+		display = Display.new(output, input)
+		display.get_rate_of_return
+		expect(output.string).to eq("Please enter a number greater than 0\n")
+	end
 
 	# it "returns 'Please enter a number greater than 0'" do
 	# 	input = StringIO.new("!")
